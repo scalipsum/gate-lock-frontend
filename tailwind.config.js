4569c46590b86870b9ba427d0d1a3af9eec9c050
Vlad Bibire <vladbbr1@gmail.com>
2022-12-16 20:12:25 +0200

    Switch from Electro to CRA

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,8 @@
-const colors = require('tailwindcss/colors');
-
 module.exports = {
-  content: ['./src/renderer/**/*.{js,jsx,ts,tsx,ejs}'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      blue: '#000',
-      purple: '#7e5bef',
-      pink: '#ff49db',
-      orange: '#ff7849',
-      green: '#13ce66',
-      yellow: '#ffc82c',
-      'gray-dark': '#273444',
-      gray: '#8492a6',
-      'gray-light': '#d3dce6',
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
-    extend: {
-      spacing: {
-        '8xl': '96rem',
-        '9xl': '128rem',
-      },
-      borderRadius: {
-        '4xl': '2rem',
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
