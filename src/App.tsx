71e9c0443e505831f8c09b73b58597e53e267cac
Vlad Bibire <vladbbr1@gmail.com>
2023-02-16 17:33:58 +0200

    :sparkles: Tailwind config / Typography / Reset CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Hello</h1>
+			<h2>Hello</h2>
+			<h3>Hello</h3>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed semper,
+				neque ut gravida fermentum, purus magna varius ipsum, eu iaculis magna
+				enim ac tellus. Ut vel luctus lectus. Nulla facilisi. Quisque id
+				porttitor lacus. Duis pulvinar nulla leo, nec lacinia mi imperdiet eget.
+				Integer vel turpis id neque ultricies rutrum. Quisque sit amet nibh in
+				ligula fermentum finibus. Aliquam erat volutpat.
+			</p>
+			<p>
+				<strong>hello</strong>
+			</p>
+			<p>
+				<small>hello</small>
+			</p>
+			<button>hello</button>
+		</div>
+	);
 }
 
 export default App;
