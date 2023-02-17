678ce7cc4c12f25bdb1a51e135ba3cac02ff485b
Vlad Bibire <vladbbr1@gmail.com>
2023-02-17 10:22:20 +0200

    Input base

--- src/common/components/elements/input/Input.tsx
+++ src/common/components/elements/input/Input.tsx
@@ -1,14 +1,52 @@
-import React, { FC, InputHTMLAttributes } from 'react';
+import React, { FC, InputHTMLAttributes, useState } from 'react';
+import OptionalWrapper from '../wrapper/OptionalWrapper';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+	// Functional
 	name: string;
+	label?: string;
+	// Styling
 	containerClassName?: string;
+	className?: string;
+	activeColor?: string;
 }
 
-const Input: FC<InputProps> = ({ name, containerClassName, ...props }) => {
+const Input: FC<InputProps> = ({
+	// Functional
+	name,
+	label,
+	// Styling
+	className,
+	containerClassName,
+	activeColor,
+	...props
+}) => {
+	const [active, setActive] = useState<boolean>(false);
+
 	return (
-		<div className={containerClassName}>
-			<input id={name} {...props} />
+		<div className={`w-full ${containerClassName}`}>
+			{/* Label */}
+			<OptionalWrapper data={label}>
+				<label htmlFor={name} className="block mb-2">
+					{label}
+				</label>
+			</OptionalWrapper>
+			{/* Input */}
+			<div className="relative rounded-lg">
+				<div
+					className={`w-6 h-full absolute top-0 left-0 rounded-tl-lg rounded-bl-lg transition-colors duration-300 ease-in-out ${
+						active ? `bg-${activeColor ?? 'blue'}` : 'bg-gray3'
+					}`}
+				/>
+				<input
+					id={name}
+					name={name}
+					className={`py-4 pl-9 pr-4 rounded-lg w-full border border-gray2 text-black placeholder:text-gray4 text-lg`}
+					onFocus={() => setActive(true)}
+					onBlur={() => setActive(false)}
+					{...props}
+				/>
+			</div>
 		</div>
 	);
 };
--- src/pages/auth/login/login.page.tsx
+++ src/pages/auth/login/login.page.tsx
@@ -1,16 +1,21 @@
 import React, { FC } from 'react';
 import SymbolLogo from '../../../assets/svg/gl-logo-symbol.svg';
 import TextLogo from '../../../assets/svg/gl-logo-text.svg';
+import Input from '../../../common/components/elements/input/Input';
 
 const LoginPage: FC = () => {
 	return (
 		<div className="h-screen w-full flex justify-center items-center">
-			{/* Logo */}
 			<div className="flex flex-col items-center mb-32">
-				<img src={SymbolLogo} alt="Gate Lock Symbol" className="w-64" />
-				<img src={TextLogo} alt="Gate Lock Title" className="mt-6 w-64" />
+				<img src={SymbolLogo} alt="Gate Lock Symbol" className="w-80" />
+				<img src={TextLogo} alt="Gate Lock Title" className="mt-6 w-80" />
+				<Input
+					name="email"
+					placeholder="Enter your email..."
+					containerClassName="mt-16"
+					label="Email"
+				/>
 			</div>
-			{/* Input */}
 			{/* Button */}
 		</div>
 	);
